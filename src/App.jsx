c5cb98db82ef7805e498a662e7e9a2e9b6c77bce
Ka Ka Lưu <kacode357@gmail.com>
2025-01-13 10:44:15 +0700

    update project v1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import ChooseZen from "./components/ChooseZen";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 >
+    <ChooseZen />
     </h1>
   )
 }
---
+++ src/components/GenZ.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+const GenZ = () => {
+    return (
+      <div className="text-white text-center bg-blue-500 w-full h-full">
+        <Header/>
+        <p className="mt-4">This is the screen for Gen Z content.</p>
+      </div>
+    );
+  };
+  
+  export default GenZ;
+  
